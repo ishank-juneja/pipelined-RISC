68aa388ad5ae634a91eb6a500a8cb014bd40b892
neildalal <44478153+neildalal@users.noreply.github.com>
2018-11-28 12:21:49 +0530

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -42,14 +42,29 @@ type mem_array is array (0 to 2**DEPTH - 1)	of
 	std_logic_vector(WIDTH - 1 downto 0);
 
 --instance of memory
-signal RAM : mem_array;	--Array that Implements Memory
+signal RAM : mem_array := ("0110000000000000",
+"0110000000000001",
+"0110000000000010",
+"0110000001010110",
+"0110000011111111",
+"0000000001100001",
+"0001000101000011",
+"0001111111111110",
+"0110111111100001",
+"0110000001111010",
+	 	"0110011001010110",
+	 	"0110000011111111",
+		"0000000000000000",
+	   "1010101010101010", 
+	   "0000000000000000",
+		"0110000000000000");	--Array that Implements Memory
 
 
 ------- Architechture Begins------------------
 begin
 		
 --Memory thread
-synchronous_RAM: process(clk) 
+synchronous_RAM: process(clk, mem_a, RAM, en, RD) 
 	
 --variable declaration	
 variable index : integer;
@@ -59,11 +74,15 @@ begin
 	--An Asynchrnous Read task	
 	--Here Depth is 4	
 	index := to_integer(unsigned(mem_a(DEPTH downto 0)));	
-			
-	if(rising_edge(clk)) then
-		
-		if(en = '1') then
-
+	
+	if(en = '1') then		
+	
+		--Asynchronous Read of Data
+		--Read from Memory Case, WR = '0' implicit	
+		if(RD = '1') then		
+			dout <= RAM(index);		
+							
+		elsif(rising_edge(clk)) then
 			--Synchrnous Write of Data
 			--Write to Memory Case, RD = '0' implicit	
 			if(WR = '1') then
@@ -71,25 +90,24 @@ begin
 				--Explicit Assignment to prevent latch				
 				dout <= (others => '0');
 
-			--Synchrnous Read of Data
-			--Read from Memory Case, WR = '0' implicit	
-			elsif(RD = '1') then		
-				dout <= RAM(index);		
-			
 			--Memory Idle Case 
 			-- WR = 0 and RD = 0		
 			else 
 				--Explicit Assignment to prevent latch
 				dout <= (others => '0');			
-			
-			end if; 		
-		else 
-			--Chip Not Enabled
-			--Do nothing, no updation		
-		end if;
 		
+			end if;
+		
+		else 
+			--Explicit Assignment to prevent latch
+			dout <= (others => '0');			
+		end if; 		
+	else 
+		dout <= (others => '0');
+		--Chip Not Enabled
+		--Do nothing, no updation		
 	end if;
-
+	
 end process;
 
 end behav;
