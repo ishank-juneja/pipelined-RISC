32a07f44c5b7ac7e0ddfd9b2797801ad86039d9d
aslimaye <41477314+aslimaye@users.noreply.github.com>
2018-11-28 20:45:19 +0530

    Update control.vhd

--- control.vhd
+++ control.vhd
@@ -6,7 +6,6 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity control is
 	port (instruction : in std_logic_vector(15 downto 0);
-			carry,zero : in std_logic;
 			output : out std_logic_vector(15 downto 0)
 			);
 end entity;
@@ -16,13 +15,10 @@ signal x : std_logic;
 begin
 
 opcode<=instruction(15 downto 12);
-x <= '1' 	when (instruction(1 downto 0)="00") else
-		carry when (instruction(1 downto 0)="10") else
-		zero 	when (instruction(1 downto 0)="01");
 
-output<= "0100000110000110" when (opcode = "0000" and x = '1') else
+output<= "0100000110000010" when (opcode = "0000") else
 			"0100001110000110" when (opcode = "0001") else
-			"0100100010000110" when (opcode = "0010" and x = '1') else
+			"0100100010000010" when (opcode = "0010") else
 			"0000000000000111" when (opcode = "0011") else
 			"0100001011000101" when (opcode = "0100") else
 			"0101001000100000" when (opcode = "0101") else
