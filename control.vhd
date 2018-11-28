7edcc45f45f8fa496264179a7fbd1408ffeb9c59
ishank-juneja <ishankjuneja@gmail.com>
2018-11-28 20:26:52 +0530

    Control commented

--- control.vhd
+++ control.vhd
@@ -1,26 +1,32 @@
- library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 
 entity control is
-	port (instruction : in std_logic_vector(15 downto 0);
+	port (	instruction : in std_logic_vector(15 downto 0);
 			carry,zero : in std_logic;
+			----------------------------------------			
 			output : out std_logic_vector(15 downto 0)
 			);
 end entity;
+
+------Begin Architechture of Controller 
 architecture behave of control is
+
 signal opcode : std_logic_vector(3 downto 0);
 signal x : std_logic;
+
 begin
 
-opcode<=instruction(15 downto 12);
+opcode <= instruction(15 downto 12);
+--For the 3 cases of arithmetic instructions
 x <= '1' 	when (instruction(1 downto 0)="00") else
 		carry when (instruction(1 downto 0)="10") else
 		zero 	when (instruction(1 downto 0)="01");
 
-output<= "0100000110000110" when (opcode = "0000" and x = '1') else
+output <= 	"0100000110000110" when (opcode = "0000" and x = '1') else
 			"0100001110000110" when (opcode = "0001") else
 			"0100100010000110" when (opcode = "0010" and x = '1') else
 			"0000000000000111" when (opcode = "0011") else
