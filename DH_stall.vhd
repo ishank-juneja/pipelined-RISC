ac6e3400a7679553d5640c060a23abe452265202
ishank-juneja <ishankjuneja@gmail.com>
2018-11-28 13:40:26 +0530

    stage 2 modified with data hazard

--- DH_stall.vhd
+++ DH_stall.vhd
@@ -12,11 +12,9 @@ use ieee.std_logic_1164.all;
 
 entity DH_stall is
 	port (
-		rst	: in std_logic;	--Global reset, active high	
-		clk	:	in std_logic;	--on board clock
 		stage3_mem_rd : in std_logic;	--Determines if stall occurs		
-		rf_a1, rf_a2 : std_logic_vector(2 downto 0);
-		stage3_a3 : std_logic_vector(2 downto 0);
+		rf_a1, rf_a2 : in std_logic_vector(2 downto 0);
+		stage3_a3 : in std_logic_vector(2 downto 0);
 		--------------------------------- 	
 		kill_bit	: out std_logic	--kill bit to stall the pipeline, active high		
 		);  
@@ -28,13 +26,13 @@ architecture behav of stall_logic is
 begin
 
 --Sequential logic
-data_hazard_stall: process(clk) 
+data_hazard_stall: process() 
 
 begin
 	--if processor is not rst
 	--and there is a load type instruction
 	--ie a possibility of a data hazard
-	if(rst = '0' and stage3_mem_rd = '1')					
+	if(stage3_mem_rd = '1')					
 		if(rf_a1 = stage3_a3)
 			kill_bit <= '1';
 		
@@ -44,10 +42,10 @@ begin
 		else
 			kill_bit <= '0';
 		end if
-	--Don't kill if processor is in rst state	
-	--or if there is no unresolvable data dependence 	
+	--Don't kill if	there is no unresolvable data dependence 	
 	else
 		kill_bit <= '0';
 	end if
 
+end process
 end behav;
