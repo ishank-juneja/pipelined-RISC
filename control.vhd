f66a4653bdff94e2caffe0c12491aa3939823950
aslimaye <41477314+aslimaye@users.noreply.github.com>
2018-11-29 12:49:20 +0530

    Add files via upload

--- control.vhd
+++ control.vhd
@@ -1,7 +1,7 @@
- library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 
 entity control is
@@ -9,16 +9,19 @@ entity control is
 			output : out std_logic_vector(15 downto 0)
 			);
 end entity;
+
+------Begin Architechture of Controller 
 architecture behave of control is
+
 signal opcode : std_logic_vector(3 downto 0);
-signal x : std_logic;
+
 begin
 
-opcode<=instruction(15 downto 12);
+opcode <= instruction(15 downto 12);
 
-output<= "0100000110000010" when (opcode = "0000") else
+output<= "0100000110000110" when (opcode = "0000") else
 			"0100001110000110" when (opcode = "0001") else
-			"0100100010000010" when (opcode = "0010") else
+			"0100100010000110" when (opcode = "0010") else
 			"0000000000000111" when (opcode = "0011") else
 			"0100001011000101" when (opcode = "0100") else
 			"0101001000100000" when (opcode = "0101") else
